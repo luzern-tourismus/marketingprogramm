6cb5a20cc7228ba2700329b4a98d3d606c97a988
Lang Urs <urs.lang@luzern.com>
2025-06-16 09:46:31 +0200

    .

--- src/Controller/MarketingProgrammController.php
+++ src/Controller/MarketingProgrammController.php
@@ -3,22 +3,20 @@
 namespace LuzernTourismus\MarketingProgramm\Controller;
 
 
+use LuzernTourismus\MarketingProgramm\Site\Admin\VerwaltungSite;
 use LuzernTourismus\MarketingProgramm\Site\AktivitaetSite;
 use LuzernTourismus\MarketingProgramm\Site\AnmeldungDeleteSite;
 use LuzernTourismus\MarketingProgramm\Site\AnmeldungSaveSite;
 use LuzernTourismus\MarketingProgramm\Site\AnmeldungSite;
 use LuzernTourismus\MarketingProgramm\Site\HomeSite;
-use LuzernTourismus\MarketingProgramm\Site\Admin\VerwaltungSite;
-
 use LuzernTourismus\MarketingProgramm\Type\Thema\AbstractThema;
 use LuzernTourismus\MarketingProgramm\Type\Thema\BasismarketingThema;
 use LuzernTourismus\MarketingProgramm\Type\Thema\MarktmanagementThema;
 use LuzernTourismus\MarketingProgramm\Type\Thema\WerbungThema;
+use Nemundo\App\Application\Site\AdminAppSite;
 use Nemundo\App\Application\Site\AppSite;
 use Nemundo\App\UserAction\Site\UserActionSite;
-use Nemundo\Core\Converter\UrlConverter;
 use Nemundo\Core\Text\TextConverter;
-use Nemundo\Core\Type\Text\Text;
 use Nemundo\Web\Controller\AbstractWebController;
 
 class MarketingProgrammController extends AbstractWebController
@@ -33,17 +31,6 @@ class MarketingProgrammController extends AbstractWebController
             ->addThemaSite(new BasismarketingThema())
             ->addThemaSite(new MarktmanagementThema());
 
-
-        /*$site = new AktivitaetSite($this);
-        $site->thema = new BasismarketingThema();
-        $site->title = 'Basis';
-        $site->url = 'basis';
-
-        $site = new AktivitaetSite($this);
-        $site->title = 'Basis2';
-        $site->url = 'basis2';*/
-
-
         new AnmeldungSaveSite($this);
         new AnmeldungDeleteSite($this);
 
@@ -51,9 +38,8 @@ class MarketingProgrammController extends AbstractWebController
         new VerwaltungSite($this);
 
 
-
         new AppSite($this);
-        new \Nemundo\App\Application\Site\AdminSite($this);
+        new AdminAppSite($this);
         new UserActionSite($this);
 
     }
@@ -65,7 +51,7 @@ class MarketingProgrammController extends AbstractWebController
         $site = new AktivitaetSite($this);
         $site->thema = $thema;
         $site->title = $thema->thema;
-        $site->url = (new TextConverter( ))->convertToUrl($thema->thema);  // ->changeToLowercase()->getValue();  // 'basis';
+        $site->url = (new TextConverter())->convertToUrl($thema->thema);
 
         return $this;
 
